2c4ec74949f6deb272b4d9c6d4cff1a0fb296048
Mriganka Goswami <mrigankagoswami03@gmail.com>
2020-12-06 23:33:19 +0530

    connect to cloud db

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded(
   { extended: "true" }
 ));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://mrigankx:test@cluster0.khu7t.mongodb.net/todolistDB?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true });
 
 const itemSchema = new mongoose.Schema({
     name: String
